7f1f4d315fe663fdedc46b916a4e26576927d3f9
Nandi Richard <94351277+nandirchrd@users.noreply.github.com>
2022-10-17 23:36:08 +0700

    Fixed folder moving .zshrc

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ git clone https://github.com/nandirchrd/my-default-zsh.git ~/my-default-zsh
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 chsh -s zsh
+mv ~/my-default-zsh/.zshrc ~/
 mv ~/my-default-zsh/font.ttf ~/.termux
-mv ~/my-default-zsh/.zshrc ~/.termux
 mv ~/my-default-zsh/colors.properties ~/.termux
 rm -rf ~/my-default-zsh
 rm $PREFIX/etc/apt/motd
